077dc5850a94ed77fe19774461ca7ffc3d2eba1c
Denys <25481529+denys-log@users.noreply.github.com>
2025-09-03 16:21:00 +0300

    add cd

---
+++ main.tf
@@ -0,0 +1,105 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.24"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+# Kubernetes and Helm providers configuration
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "eu-north-1"]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "eu-north-1"]
+    }
+  }
+}
+
+# S3 Backend Module
+module "s3_backend" {
+  source      = "./modules/s3-backend"
+  bucket_name = "denystest-terraform-state-lesson-7"
+  table_name  = "terraform-locks"
+}
+
+# VPC Module  
+module "vpc" {
+  source             = "./modules/vpc"
+  vpc_cidr_block     = "10.0.0.0/16"
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  availability_zones = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  vpc_name           = "lesson-7-vpc"
+}
+
+# ECR Module
+module "ecr" {
+  source       = "./modules/ecr"
+  ecr_name     = "lesson-7-django-app"
+  scan_on_push = true
+}
+
+# EKS Module
+module "eks" {
+  source = "./modules/eks"
+  
+  cluster_name    = "lesson-7-eks-cluster"
+  cluster_version = "1.28"
+  
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = concat(module.vpc.private_subnet_ids, module.vpc.public_subnet_ids)
+  
+  node_group_name         = "lesson-7-nodes"
+  node_group_capacity     = "t3.medium"
+  node_group_min_size     = 2
+  node_group_max_size     = 6
+  node_group_desired_size = 2
+}
+
+# Jenkins Module
+module "jenkins" {
+  source = "./modules/jenkins"
+  
+  cluster_name     = module.eks.cluster_name
+  cluster_endpoint = module.eks.cluster_endpoint
+  namespace        = "jenkins"
+}
+
+# Argo CD Module  
+module "argo_cd" {
+  source = "./modules/argo_cd"
+  
+  cluster_name     = module.eks.cluster_name
+  cluster_endpoint = module.eks.cluster_endpoint
+  namespace        = "argocd"
+}
